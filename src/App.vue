e672b11adb2e6bde059cc4c6dcff339e155f92c1
Jose Luis Quispe <joseluisstacruz@gmail.com>
2025-01-31 22:42:33 -0400

    first commit and init project with first page

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Button from './components/Button.vue'
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center w-full h-dvh">
+    <h1 class="m-10 text-7xl font-mono text-amber-500">DAWN CODE</h1>
+    <div
+      class="container bg-gradient-to-t from-black to-amber-500 pl-4 pr-4 pb-2 pt-2 bg-amber-950 flex flex-col items-center justify-center">
+      <Button msg="Code 1" />
+      <Button msg="Great Code 2" />
+      <Button msg="Code 3" />
+      <Button msg="Code 4" />
+      <Button msg="Code 5" />
+    </div>
+  </div>
+
+</template>
---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+const props = defineProps<{ msg: string }>()
+
+const goToPage = () => {
+  console.log(props.msg.toLowerCase())
+}
+
+</script>
+
+<template>
+  <div class="card">
+    <button class="m-1 bg-amber-600 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded cursor-pointer "
+      type="button" @click="goToPage">{{ msg
+      }}</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
