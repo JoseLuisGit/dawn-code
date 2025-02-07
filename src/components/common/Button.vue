1503a3c6ecb4c569c334419b2afa9e83ef1ebc29
Jose Luis Quispe <joseluisstacruz@gmail.com>
2025-02-06 22:21:24 -0400

    improvements in home page

--- src/components/common/Button.vue
+++ src/components/common/Button.vue
@@ -4,7 +4,7 @@ defineProps<{ title: string }>()
 
 <template>
   <div class="card">
-    <button class="m-1 bg-amber-600 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded cursor-pointer "
+    <button class="m-1 bg-blue-900 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded cursor-pointer "
       @click="$router.push(title.toLowerCase().replace(' ', '-'))">
       {{ title }}
     </button>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,9 +3,9 @@ import Button from '../components/common/Button.vue'
 </script>
 <template>
     <div class="flex flex-col items-center justify-center w-full h-dvh">
-        <h1 class="m-10 text-7xl font-mono font-bold text-amber-950">DAWN CODE</h1>
+        <h1 class="m-10 text-7xl font-mono font-bold text-black">DAWN CODE</h1>
         <div
-            class="container bg-clip-border bg-gradient-to-tl from-orange-950 to-orange-400 pl-4 pr-4 pb-2 pt-2 flex flex-col items-center justify-center">
+            class="container h-full bg-clip-border bg-gradient-to-tl from-35%-950 to-cyan-700 pl-4 pr-4 pb-2 pt-2 flex flex-col justify-start">
             <Button title="Todo List" />
         </div>
     </div>
