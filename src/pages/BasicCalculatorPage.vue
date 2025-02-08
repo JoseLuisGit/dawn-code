f5d172031914819725e0c46258f4c00b5d26858d
Jose Luis Quispe <joseluisstacruz@gmail.com>
2025-02-08 09:53:43 -0400

    feat: implement core functionality for Basic Calculator with input handling and operations

--- src/pages/BasicCalculatorPage.vue
+++ src/pages/BasicCalculatorPage.vue
@@ -1,3 +1,69 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+
+let mainInput = ref<string>('0')
+
+let history = ref<string>('')
+let result: number = 0
+
+let hasOperation = ref<boolean>(false)
+
+function addInput(value: string) {
+    if (mainInput.value === '0' && value !== '.') {
+        mainInput.value = ''
+    }
+    if (hasOperation.value) {
+        mainInput.value = ''
+        hasOperation.value = false
+    }
+    mainInput.value += value
+}
+
+function deleteLeft() {
+    mainInput.value = mainInput.value.slice(0, -1)
+}
+
+function addOperation(value: string) {
+    history.value += mainInput.value + ' ' + value + ' '
+    hasOperation.value = true
+
+    if (result === 0) {
+        result = Number(mainInput.value)
+        return;
+    }
+
+    switch (value) {
+        case '+':
+            result = result + Number(mainInput.value)
+            break;
+        case '-':
+            result = result - Number(mainInput.value)
+            break;
+        case 'x':
+            result = result * Number(mainInput.value)
+            break;
+        case '/':
+            result = result / Number(mainInput.value)
+            break;
+    }
+
+    mainInput.value = String(result)
+}
+
+function reset() {
+    mainInput.value = '0'
+    history.value = ''
+    result = 0
+}
+
+function resetMainInput() {
+    mainInput.value = '0'
+}
+
+</script>
+
 <template>
     <div class="bg-gradient-to-tl from-gray-950 to-gray-700 h-screen flex flex-col items-center justify-center w-full">
         <section class="container bg-white h-140 max-w-90 rounded-xl pr-3 pl-3 pt-4 border-gray-400 border-2">
@@ -5,9 +71,11 @@
                 <h1 class="text-xl text-gray-500 font-normal">Basic Calculator</h1>
                 <font-awesome-icon :icon="['fas', 'bars']" class="ml-auto mr-2 cursor-pointer" />
             </div>
-            <div class="container bg-white h-15 border-gray-400 border rounded-b-md rounded-t-md mb-5">
-                <h2 class="text-xs text-gray-500 text-right mr-2">3,500 x 3 =</h2>
-                <h1 class="text-3xl text-gray-900 font-semibold text-right mr-2 leading-5">10,500</h1>
+            <div class="container bg-gray-100 h-15 border-gray-400 border rounded-b-md rounded-t-md mb-5">
+                <div class="h-4">
+                    <h2 class="text-md text-gray-500 text-right mr-2">{{ history }}</h2>
+                </div>
+                <h1 class="text-4xl text-gray-900 font-semibold text-right mr-2">{{ mainInput }}</h1>
             </div>
             <div class="grid grid-cols-5 gap-2">
                 <button
@@ -19,12 +87,14 @@
                 <button
                     class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-red-700 hover:bg-red-800 text-white">MR</button>
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-300 hover:bg-gray-400 text-black"><font-awesome-icon
-                        :icon="['fas', 'delete-left']" /></button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-300 hover:bg-gray-400 text-black"
+                    @click="deleteLeft"><font-awesome-icon :icon="['fas', 'delete-left']" /></button>
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-300 hover:bg-gray-400 text-black">AC</button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-300 hover:bg-gray-400 text-black"
+                    @click="reset">AC</button>
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-300 hover:bg-gray-400 text-black">CE</button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-300 hover:bg-gray-400 text-black"
+                    @click="resetMainInput">CE</button>
 
                 <button
                     class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-300 hover:bg-gray-400 text-black"><font-awesome-icon
@@ -38,51 +108,62 @@
                     class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-bold text-3xl bg-gray-300 hover:bg-gray-400 text-black">^2</button>
 
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black">7</button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black"
+                    @click="addInput('7')">7</button>
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black">8</button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black"
+                    @click="addInput('8')">8</button>
 
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black">9</button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black"
+                    @click="addInput('9')">9</button>
 
                 <button
                     class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-bold text-3xl bg-gray-300 hover:bg-gray-400 text-black"><font-awesome-icon
-                        :icon="['fas', 'divide']" /></button>
+                        :icon="['fas', 'divide']" @click="addOperation('/')" /></button>
                 <button
                     class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-bold text-3xl bg-gray-300 hover:bg-gray-400 text-black"><font-awesome-icon
                         :icon="['fas', 'percent']" /></button>
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black">4</button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black"
+                    @click="addInput('4')">4</button>
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black">5</button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black"
+                    @click="addInput('5')">5</button>
 
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black">6</button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black"
+                    @click="addInput('6')">6</button>
                 <button
                     class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-bold text-3xl bg-gray-300 hover:bg-gray-400 text-black"><font-awesome-icon
-                        :icon="['fas', 'xmark']" /></button>
+                        :icon="['fas', 'xmark']" @click="addOperation('x')" /></button>
                 <button
                     class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-semibold text-3xl bg-gray-300 hover:bg-gray-400 text-black">1/x</button>
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black">1</button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black"
+                    @click="addInput('1')">1</button>
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black">2</button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black"
+                    @click="addInput('2')">2</button>
 
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black">3</button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black"
+                    @click="addInput('3')">3</button>
                 <button
                     class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-bold text-3xl bg-gray-300 hover:bg-gray-400 text-black"><font-awesome-icon
-                        :icon="['fas', 'minus']" /></button>
+                        :icon="['fas', 'minus']" @click="addOperation('-')" /></button>
                 <button
                     class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-bold text-3xl bg-blue-400 hover:bg-blue-500 text-white col-span-1 row-span-2"><font-awesome-icon
                         :icon="['fas', 'equals']" /></button>
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black row-span-1 col-span-2">0</button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-medium text-3xl bg-gray-100 hover:bg-gray-300 text-black row-span-1 col-span-2"
+                    @click="addInput('0')">0</button>
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-bold text-3xl bg-gray-100 hover:bg-gray-300 text-black">.</button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-bold text-3xl bg-gray-100 hover:bg-gray-300 text-black"
+                    @click="addInput('.')">.</button>
                 <button
-                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-bold text-3xl bg-gray-300 hover:bg-gray-400 text-black"><font-awesome-icon
-                        :icon="['fas', 'plus']" /></button>
+                    class="px-2 py-3 border border-gray-100 rounded cursor-pointer font-bold text-3xl bg-gray-300 hover:bg-gray-400 text-black"
+                    @click="addOperation('+')"><font-awesome-icon :icon="['fas', 'plus']" /></button>
             </div>
         </section>
     </div>
