c90695fbd3473a1622adfa100cf3032693305766
Jose Luis Quispe <joseluisstacruz@gmail.com>
2025-02-21 23:43:57 -0400

    feat: add Digital Clock page with custom font and styling

--- src/pages/DigitalClockPage.vue
+++ src/pages/DigitalClockPage.vue
@@ -1,4 +1,58 @@
 <template>
     <div class="bg-gradient-to-tl from-gray-950 to-gray-700 h-screen flex flex-col items-center justify-center w-full">
+        <section
+            class="border-8 border-gray-900 bg-black w-100 p-4 rounded-lg shadow-lg flex flex-col items-center justify-center">
+            <div class="text-8xl font-digital text-red-600 pb-2 flex flex-row items-center justify-center">
+                <div class="bg-red-950/35 p-1">
+                    16
+                </div>
+                <p>:</p>
+                <div class="bg-red-950/35 p-1">
+                    50
+                </div>
+                <p>:</p>
+                <div class="bg-red-950/35 p-1">
+                    20
+                </div>
+            </div>
+            <div class="text-xs text-white flex flex-row pb-2 font-semibold font-sans">
+                <p class="text-white p-0.5">SUN</p>
+                <p class="text-white p-0.5">MON</p>
+                <p class="text-white p-0.5">TUE</p>
+                <p class="text-red-600 p-0.5">WED</p>
+                <p class="text-white p-0.5">THU</p>
+                <p class="text-white p-0.5">FRI</p>
+                <p class="text-white p-0.5">SAT</p>
+            </div>
+            <div class="text-md text-white flex flex-row item-center font-sans">
+                <div class="flex flex-row pr-4">
+                    <p class="text-red-600 font-digital text-xl
+                    p-0.5">19</p>
+                    <p class="text-white
+                    p-0.5">DATE</p>
+                </div>
+                <div class="flex flex-row pr-4">
+                    <p class="text-red-600 font-digital text-xl
+                    p-0.5">9</p>
+                    <p class="text-white
+                    p-0.5">MON</p>
+                </div>
+                <div class="flex flex-row pr-4">
+                    <p class="text-red-600 font-digital text-xl
+                    p-0.5">2019</p>
+                    <p class="text-white
+                    p-0.5">YEAR</p>
+                </div>
+                <div class="flex flex-row pr-4">
+                    <p class="text-white
+                    p-0.5">TEMP</p>
+                    <p class="text-red-600 font-digital text-xl
+                    p-0.5">28</p>
+                    <p class="text-white">°C</p>
+                </div>
+            </div>
+
+        </section>
+
     </div>
 </template>
